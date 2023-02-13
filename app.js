f8da70351a72bead457e082705b0b291c2c69d91
Basanta Pandey <35844004+basanta-7@users.noreply.github.com>
2023-02-13 22:51:00 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+function hideMain1(){
+    document.getElementById('main1').style.display = 'none';
+    document.getElementById('main2').style.display = 'block';
+}
+
+function hideMain2(){
+    document.getElementById('main1').style.display = 'none';
+    document.getElementById('main2').style.display = 'none';
+    document.getElementById('main3').style.display = 'block';
+}
+
+function hideMain3(){
+    document.getElementById('main1').style.display = 'none';
+    document.getElementById('main2').style.display = 'none';
+    document.getElementById('main3').style.display = 'none';
+    document.getElementById('main4').style.display = 'block';
+}
+
+function hideMain4(){
+    document.getElementById('main1').style.display = 'none';
+    document.getElementById('main2').style.display = 'none';
+    document.getElementById('main3').style.display = 'none';
+    document.getElementById('main4').style.display = 'none';
+    document.getElementById('main5').style.display = 'block';
+}
+
+function hideMain5(){
+    document.getElementById('main1').style.display = 'none';
+    document.getElementById('main2').style.display = 'none';
+    document.getElementById('main3').style.display = 'none';
+    document.getElementById('main5').style.display = 'none';
+    document.getElementById('main6').style.display = 'block';
+}
+
+function hideMain6(){
+    document.getElementById('main1').style.display = 'none';
+    document.getElementById('main2').style.display = 'none';
+    document.getElementById('main3').style.display = 'none';
+    document.getElementById('main5').style.display = 'none';
+    document.getElementById('main6').style.display = 'none';
+    document.getElementById('main7').style.display = 'block';
+}
